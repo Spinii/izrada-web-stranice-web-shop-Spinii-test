97964c1aff307f4a1399aeeeaf013fc34f05f60e
VPapic <vjekopapic55@gmail.com>
2024-02-21 19:36:21 +0100

    Update linkovi

--- js/whiteHeader.js
+++ js/whiteHeader.js
@@ -4,5 +4,5 @@ xhttp.onload = function(){
     document.getElementById("header").innerHTML = this.responseText
 }
 
-xhttp.open("GET", "../partials/shopheader.html");
+xhttp.open("GET", "partials/shopheader.html");
 xhttp.send();
